f81027a78a0c3c7ee45f05665ed2b1430f9ef1ae
Okoeguale Godwin Kelvin <okoegualegodwin@gmail.com>
2023-03-17 02:10:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prirnts the alphabet in lower case.
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	char low_case = "a";
+
+	while (low_case <= 'z')
+	{
+		putchar(low_case);
+		low_case++;
+	}
+	putchar('\n');
+	return (0);
+}
