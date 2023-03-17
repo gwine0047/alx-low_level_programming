7bcf30225fc094b6a0c16ebd280e0969308a943b
Okoeguale Godwin Kelvin <okoegualegodwin@gmail.com>
2023-03-17 03:17:31 -0700

    almost

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - prints all single digits using only putcha
+* Return: 0 always (Success)
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	putchar (num + '0');
+
+	putchar ('\n');
+	return (0);
+}
