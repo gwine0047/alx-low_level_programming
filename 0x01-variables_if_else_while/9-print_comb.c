b6afbc56538a79ce3928afa2d67d9bad4be2b016
Okoeguale Godwin Kelvin <okoegualegodwin@gmail.com>
2023-03-17 04:07:40 -0700

    done

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible combinations of single digit numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar (48 + num);
+		if (num != 9)
+		{
+			putchar (',');
+			putchar (' ');
+		}
+		num++;
+	}
+	putchar ('\n');
+	return (0);
+}
