f3918bdc1dc6da111f37b5fa0777ef06e43d36e3
Okoeguale Godwin Kelvin <okoegualegodwin@gmail.com>
2023-03-18 09:25:13 -0700

    gitty

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,36 +16,35 @@
  */
 int main(void)
 {
-	int a;
-	int b;
-	int c = 0;
+int a;
+int b;
+int c = 0;
 
-	while (c < 10)
-	{
-		b = 0;
-		while (b < 10)
-		{
-			a = 0;
-			while (a < 0)
-			{
-				if (a != b && b != c && c < b && b < a)
-				{
-					putchar('0' + c);
-					putchar('0' + b);
-					putchar('0' + a);
-
-					if (a + b + c != 24)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
-			a++;
-			}
-		b++;
-		}
-	c++;
-	}
-	putchar('\n');
-	return (0);
+while (c < 10)
+{
+b = 0;
+while (b < 10)
+{
+a = 0;
+while (a < 0)
+{
+if (a != b && b != c && c < b && b < a)
+{
+putchar('0' + c);
+putchar('0' + b);
+putchar('0' + a);
+if (a + b + c != 24)
+{
+putchar(',');
+putchar(' ');
+}
+}
+a++;
+}
+b++;
+}
+c++;
+}
+putchar('\n');
+return (0);
 }
