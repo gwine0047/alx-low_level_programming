5730d8e24c7e354ab1d10cb37c27ab26920d9345
Okoeguale Godwin Kelvin <okoegualegodwin@gmail.com>
2023-03-21 04:03:19 -0700

    3-islower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,14 @@
-#include <stdio.h>
 #include "main.h"
-
-void print_alphabet(void);
+/**
+ * print_alphabet - is a function that returns an alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
 {
-	
+	char m;
+
+	for (m = 'a'; m <= 'z'; m++)
+		_putchar(m);
+	_putchar('\n');
 }
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - this function prints the alphabet 10 times
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int z;
+	char y;
+
+	for (z = 1; z <= 10; z++)
+	{
+		for (y = 'a'; y <= 'z'; y++)
+		
+			_putchar(y);
+		_putchar('\n');
+
+	}
+}
