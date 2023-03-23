ae79b3e4ecb6cfe34cf7ce58666a0b1c8f050c80
Okoeguale Godwin Kelvin <okoegualegodwin@gmail.com>
2023-03-22 23:36:55 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - multiplies two given integers
+ * @a: first integer
+ * @b: second integer
+ * Return: 0 Always
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
