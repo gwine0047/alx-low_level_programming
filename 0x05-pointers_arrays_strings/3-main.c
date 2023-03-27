d5f0fe7745531de48bca50720691524f6fe5a509
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-03-27 21:10:26 +0000

    pointers

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -8,8 +8,16 @@
 int main(void)
 {
 	char *str;
+	char *lorem;
+
+	lorem = "- [Got]lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
 	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 	puts(str);
+
+	_putchar('\n');
+
+	puts(lorem);
+
 	return (0);
 }
