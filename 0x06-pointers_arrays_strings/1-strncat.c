2397efe2cf42c080192d89290ba03a86aa529664
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-03-29 21:06:27 +0000

    more pointers

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,13 +9,17 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int dest_len = 0, i;
 
-	while (dest[index++])
+	for (i = 0; dest[i] != '\0'; i++)
 		dest_len++;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
 
-	return (0);
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+
+	/*dest[dest_len + i] = '\0';*/
+	return (dest);
 }
