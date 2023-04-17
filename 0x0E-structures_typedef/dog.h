ed89b0e3e26827507ea58c1963679a76ad500e95
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-04-17 16:25:01 +0000

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _BOARDER_
+#define _BOARDER_
+/**
+* struct dog - Dog features
+* @name: First member
+* @age: Second member
+* @owner: Third member
+*
+* Description: Longer description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
