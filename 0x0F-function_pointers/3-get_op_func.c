c6a6168cab10a7e6788e74f192aad2e77a82d093
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-04-19 22:22:51 +0000

    betty checked

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i].op)
 	{
 		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
+			return (ops[i].f);
 		i++;
 	}
 
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -3,23 +3,11 @@
 #include <stdio.h>
 
 /**
- * op_add - ...
- * @a: ...
- * @b: ...
+ * op_sub - subtraction
+ * @a:1st num
+ * @b:2nd num
  *
- * Return: ...
- */
-int op_add(int a, int b)
-{
-	return (a + b);
-}
-
-/**
- * op_sub - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ * Return: difference
  */
 int op_sub(int a, int b)
 {
@@ -27,11 +15,23 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - ...
- * @a: ...
- * @b: ...
+ * op_add - addition
+ * @a:1st num
+ * @b:2nd num
  *
- *  Return: ...
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a:2nd num
+ * @b:1st num
+ *
+ *  Return:multiple
  */
 int op_mul(int a, int b)
 {
@@ -39,29 +39,11 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - ...
- * @a: ...
- * @b: ...
+ * op_mod - division
+ * @a: 2nd num
+ * @b: 1st num
  *
- *  Return: ...
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	return (a / b);
-}
-
-/**
- * op_mod - ...
- * @a: ...
- * @b: ...
- *
- * Return: ...
+ *  Return: remainder
  */
 int op_mod(int a, int b)
 {
@@ -72,4 +54,22 @@ int op_mod(int a, int b)
 	}
 
 	return (a % b);
+}
+
+/**
+ * op_div - division
+ * @a: second number
+ * @b: first number
+ *
+ * Return: whole number
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	return (a / b);
 }
