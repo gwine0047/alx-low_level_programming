51b23a581d987c2f6a82deaad454d914098bce16
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-04-19 20:32:36 +0000

    num 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * @argc: arguement count
+ * @argv: arguement elements
+ * Return: 0 when successful
+ */
+int main(int argc, char *argv[])
+{
+	int b, mem = 0;
+	char *array;
+
+	if (argc !=2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	mem = atoi(argv[1]);
+
+	if (!(mem > 0))
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	array = (char *)main;
+
+	while (b < mem)
+	{
+		printf("%02hhx\n", array[b]);
+		break;
+		b++;
+	}
+	return (0);
+}
