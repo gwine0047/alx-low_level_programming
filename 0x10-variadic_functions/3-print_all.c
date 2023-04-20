66b5e21be14b929921a06e468a2f4f1b7ad69f15
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-04-20 15:05:54 +0000

    done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 /**
  * print_all - prints anything.
- * @format - is the types of arguements
+ * @format: is the types of arguements
  * Return: nothing.
  */
 void print_all(const char * const format, ...)
@@ -21,22 +21,18 @@ void print_all(const char * const format, ...)
 				case 'i':
 					printf("%s%d", com, va_arg(type, int));
 					break;
-
 				case 'c':
 					printf("%s%c", com, va_arg(type, int));
 					break;
-
 				case 'f':
 					printf("%s%f", com, va_arg(type, double));
 					break;
-
 				case 's':
 					cpy = va_arg(type, char *);
 					if (!cpy)
 						cpy = "(nil)";
 					printf("%s%s", com, cpy);
 					break;
-
 				default:
 					i++;
 					continue;
