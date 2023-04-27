9a59c8dc0de4b0f1cedf1eff4d51dcc6f2880809
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-04-27 15:55:24 +0000

    correct betty errors

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,14 @@
 #include <string.h>
 #include <stdarg.h>
 
+/**
+* struct list_s - singly linked list
+* @str: string - (malloc'ed string)
+* @len: length of the string
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+*/
 typedef struct list_s
 {
 	char *str;
@@ -20,13 +28,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-/**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed string)
-* @len: length of the string
-* @next: points to the next node
-
-* Description: singly linked list node structure
-*/
-	
 #endif
