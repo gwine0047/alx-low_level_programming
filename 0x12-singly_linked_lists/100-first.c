dbbb5bfbb5fcfae9f8cfe500c992d80f9cbaaf0d
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-04-27 17:04:30 +0000

    task 100 completed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * before_main - prints a string before executing the main function.
+ * Return: nothing.
+ */
+
+void __attribute__((constructor)) before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
