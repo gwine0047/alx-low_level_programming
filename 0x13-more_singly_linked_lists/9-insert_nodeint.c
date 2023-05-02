10c7d8bd8d5e99802b3620f74903b6d3e296138f
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-02 16:42:41 +0000

    modify code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx != 0)
 	{
-		while (count < (idx - 1) && temp->next != NULL)
+		while (count < (idx - 1) && temp != NULL)
 		{
 			temp = temp->next;
 			count++;
