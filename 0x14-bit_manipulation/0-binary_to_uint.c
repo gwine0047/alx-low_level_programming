982c073137f31f8bdf961e47f090eefe76b84c56
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-04 11:01:02 +0000

    task 0 completed

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * binary_to_unit - converts a binary to an integer.
+ * @b: is a pointer to characters.
+ *
+ * Return: the converted number
+ * or 0 if NULL.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	size_t a;
+	size_t result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		if (b[a] == '0')
+			result <<= 1;
+
+		else if (b[a] == '1')
+			result = (result << 1) | 1;
+
+		else
+			return (0);			
+	}
+	return (result);
+	printf("\n");
+}
