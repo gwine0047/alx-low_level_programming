cbfe1b74da3510bc4e1f0a205a8ca914005b5af5
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-04 12:49:28 +0000

    code modified in task 2

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,27 @@
 #include "main.h"
 /**
+ * _exponent - calculates the value of a number to 
+ * the power of n.
+ * @base: base number.
+ * @n: exponential value.
+ * 
+ * Return: the the number multiplied by itself n times.
+ */
+
+unsigned long int _exponent(unsigned int base, unsigned n)
+{
+	unsigned long int total;
+	size_t i = 0;
+
+	while (i < n)
+	{
+		total = total * base;
+		i++
+	}
+	return (total);
+}
+
+/**
  * print_binary - prints the binary representation of a number.
  * @n: is the number to be converted.
  *
@@ -7,15 +29,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	unsigned long int a, result;
+
+	a = _exponent(2, 63);
+
+	while (a != 0)
 	{
-		print_binary(n >> 1);
+		result = n & a;
+
+		if (result == a)
+		{
+			_putchar('1');
+		}
+
+		else
+		{
+			_putchar('0');
+		}
+		a >>= 1
 	}
-	else
-	{
-		_putchar('0');
-		return;
-	}
-	_putchar((n & 1) ? '1' : '0');
-	_putchar('\0');
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -17,5 +17,5 @@ int get_bit(unsigned long int n, unsigned int index)
 	mask = 1UL << index;
 	result = n & mask;
 
-	return ((result != 0) ? 1 :0);
+	return ((result != 0) ? 1 : 0);
 }
