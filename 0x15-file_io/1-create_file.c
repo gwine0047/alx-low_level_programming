c8a00faebadc83cfb141906cc4bf4da7a0486213
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-08 21:17:41 +0000

    code modified

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (f_ptr == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (!text_content)
 		text_content = "";
 
 	while (text_content[let_count] != '\0')
