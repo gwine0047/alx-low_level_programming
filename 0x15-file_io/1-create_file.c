a3bc35ae67336fc82b78ad978d8314fd3f79d933
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-09 19:23:28 +0000

    change text_content condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,14 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f_ptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, "wr-------");
-	if (f_ptr == -1)
-		return (-1);
-
 	if (!text_content)
 		text_content = "";
 
-	for (;text_content[let_count] != '\0'; let_count++);
+	for (;text_content[let_count]; let_count++);
+
+	f_ptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (f_ptr == -1)
+		return (-1);
 
 	wr_count = write(f_ptr, text_content, let_count);
 	
