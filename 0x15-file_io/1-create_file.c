f0cbc40abf0449688561215250b162aa2cb913c0
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-09 19:52:52 +0000

    fix betty complaints

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,16 +16,17 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (;text_content[let_count]; let_count++);
+		for (; text_content[let_count];)
+			let_count++;
 	}
 
 	f_ptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	wr_count = write(f_ptr, text_content, let_count);
-	
+
 	if (wr_count == -1 || f_ptr == -1)
 		return (-1);
-	
+
 	close(f_ptr);
 
 	return (1);
