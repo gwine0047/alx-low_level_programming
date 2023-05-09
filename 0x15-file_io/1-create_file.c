3c28f82993ba444422447f2b626c0862dd89511c
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-09 19:36:00 +0000

    add loop into if condition

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,10 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-
-	for (;text_content[let_count]; let_count++);
+	if (text_content != NULL)
+	{
+		for (;text_content[let_count]; let_count++);
+	}
 
 	f_ptr = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (f_ptr == -1)
