830e2b7b129e5caed9fb46f87d54f896bf417419
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-05-11 15:47:14 +0000

    complte task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,23 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
-/**
- * main - check the code
- *@ac: first argument.
- *@av: second argument.
- * Return: Always 0.
- */
-int main(int ac, char **av)
+int open_read(char *filename)
 {
-	int res;
+	int rd_fd;
 
-	if (ac != 3)
+	rd_fd = open(filename, O_RDONLY);
+	if (rd_fd == -1)
 	{
-		dprintf(2, "Usage: %s filename text\n", av[0]);
-		exit(1);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", filename);
+		exit(98);
 	}
-	res = append_text_to_file(av[1], av[2]);
-	printf("-> %i)\n", res);
+	return (rd_fd);
+}
+
+
+
+int open_write(char *filename)
+{
+	int wr_fd;
+
+	wr_fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (wr_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", filename);
+		exit(99);
+	}
+	return (wr_fd);
+} 
+
+
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, close_err;
+	ssize_t r_bytes, w_bytes;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,"%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+
+	file_from = open_read(argv[1]);
+	file_to = open_write(argv[2]);
+	
+	r_bytes = 1024;
+	while (r_bytes == 1024)
+	{
+		r_bytes = read(file_from, buffer, 1024);
+		if (r_bytes == -1)
+		{
+			dprintf(STDERR_FILENO,"Error: can't read from %s\n", argv[1]);
+		}
+
+		w_bytes = write(file_to, buffer, r_bytes);
+		if (r_bytes != w_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close_err = close(file_from);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+		exit(100);
+	}
+	close_err = close(file_to);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
+		exit(100);
+	}
 	return (0);
+
 }
