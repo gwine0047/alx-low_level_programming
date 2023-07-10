6f08ed2f9c237b3538a8436d3b9f22f09153b7ae
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-07-10 08:27:47 +0000

    modify loop in task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * read_textfile - reads a text file and prints it.
+ * @filename: is the name of the file to be read.
+ * @letters: is the number of letters.
+ *
+ * Return: the number of characters written.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	unsigned int rd_count, wr_count;
+	int f_ptr;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	f_ptr = open(filename, O_RDONLY);
+	if (f_ptr == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * (letters));
+	if (buff == NULL)
+		return (0);
+
+	rd_count = read(f_ptr, buff, letters);
+	wr_count = write(STDOUT_FILENO, buff, rd_count);
+
+	close(f_ptr);
+
+	free(buff);
+
+	return (wr_count);
+
+
+}
---
+++ 0x15-file_io/three.c
@@ -0,0 +1,90 @@
+#include "main.h"
+/**
+ * open_read - opens a file for reading.
+ * @filename: is the name of file.
+ *
+ * Return: file descriptor.
+ */
+
+int open_read(char *filename)
+{
+	int rd_fd;
+
+	rd_fd = open(filename, O_RDONLY);
+	if (rd_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", filename);
+		exit(98);
+	}
+	return (rd_fd);
+}
+/**
+ * open_write - opens a file for writing.
+ * @filename: is the name of file.
+ *
+ * Return: file descriptor.
+ */
+
+
+int open_write(char *filename)
+{
+	int wr_fd;
+
+	wr_fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	if (wr_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", filename);
+		exit(99);
+	}
+	return (wr_fd);
+}
+/**
+ * main - checks the code.
+ * @argc: commandline argument count.
+ * @argv: commandline arguments.
+ *
+ * Return: 0 when successful.
+ */
+
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, close_err;
+	ssize_t r_bytes, w_bytes;
+	char buffer[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		exit(97);
+	}
+	file_from = open_read(argv[1]);
+	file_to = open_write(argv[2]);
+	r_bytes = 1024;
+	while (r_bytes == 1024)
+	{
+		r_bytes = read(file_from, buffer, 1024);
+		if (r_bytes == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't read from %s\n", argv[1]);
+		}
+		w_bytes = write(file_to, buffer, r_bytes);
+		if (r_bytes != w_bytes)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close_err = close(file_from);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_from);
+		exit(100);
+	}
+	close_err = close(file_to);
+	if (close_err == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fd %d\n", file_to);
+		exit(100);
+	}
+	return (0);
+}
