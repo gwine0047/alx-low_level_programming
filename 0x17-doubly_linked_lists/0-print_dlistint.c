b73999d3dabfb780784035d78693d38dd2b20d00
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-17 14:56:58 +0100

    add 0-print_dlistint.c file

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+*print_dlistint - prints the elements of a
+* dlistint_t list.
+*@h: head of the list
+*Return: node count.
+*/
+size_t print_dlistint(const dlistint_t *h)
+{
+int node_count = 0;
+
+if (h == NULL)
+return (node_count);
+while (h)
+{
+node_count++;
+printf("%d\n", h->n);
+h = h->next;
+}
+return (node_count);
+}
