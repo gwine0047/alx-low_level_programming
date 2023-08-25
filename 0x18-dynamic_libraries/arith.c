e97d1943ac6a4e602190ca1e35d74db9be03f209
Okoeguale Godwin <okoegualegodwin@gmail.com>
2023-08-25 19:37:51 +0100

    make files betty compliant

--- 0x18-dynamic_libraries/arith.c
+++ 0x18-dynamic_libraries/arith.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
-
+/**
+*add - operates on numbers 
+* @a: first number
+* @b: second number
+* Return: an integer
+*/
 int add(int a, int b)
 {
 return (a + b);
 }
 
+/**
+*sub - operates on numbers 
+* @a: first number
+* @b: second number
+* Return: an integer
+*/
+
 int sub(int a, int b)
 {
-return (a + b);
+return (a - b);
 }
 
+/**
+*div - operates on numbers 
+* @a: first number
+* @b: second number
+* Return: an integer
+*/
 int div(int a, int b)
 {
 if (b != 0 )
@@ -18,11 +36,23 @@ else
     return (0);
 }
 
+/**
+*mul - operates on numbers 
+* @a: first number
+* @b: second number
+* Return: an integer
+*/
 int mul(int a, int b)
 {
 return (a * b);
 }
 
+/**
+*mod - operates on numbers 
+* @a: first number
+* @b: second number
+* Return: an integer
+*/
 int mod(int a, int b)
 {
 return (a % b);
